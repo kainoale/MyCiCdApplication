faa80a661a8d2bc547634b11e2fc78bda766f4f4
aleks <aleksandrs.kainovs@gmail.com>
2025-11-20 10:48:30 +0200

    scanBasePackages is added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,10 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "org.example",
+        "org.spring.examples"
+})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
